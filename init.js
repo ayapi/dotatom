a9e59dcb6ed3f3de30fcba12ec2f6d06c1eff6b3
ayapi <colors.aya@gmail.com>
2016-08-16 16:46:37 +0900

    ref #8 change neighbor pane detection logic

--- init.js
+++ init.js
@@ -151,16 +151,118 @@ atom.commands.add('atom-workspace', 'custom:split-global-right-and-new-editor', 
   atom.workspace.open();
 });
 
+function neighborPaneViewInDirection(direction) {
+  let pane = atom.workspace.getActivePane();
+  let paneView = atom.views.getView(pane);
+  let box = paneView.getBoundingClientRect();
+  let paneContainerView = atom.views.getView(pane.container);
+  let paneViews = Array.from(paneContainerView.querySelectorAll('atom-pane'));
+  
+  function compareVerticalDistance (a, b) {
+    let origin = box.top + (box.bottom - box.top) / 2;
+    let aDistance = Math.abs((a.top + (a.bottom - a.top) / 2) - origin);
+    let bDistance = Math.abs((b.top + (b.bottom - b.top) / 2) - origin);
+    if (aDistance <= bDistance) {
+      return a;
+    }
+    return b;
+  }
+  function compareHorizontalDistance (a, b) {
+    let origin = box.left + (box.right - box.left) / 2;
+    let aDistance = Math.abs((a.left + (a.right - a.left) / 2) - origin);
+    let bDistance = Math.abs((b.left + (b.right - b.left) / 2) - origin);
+    if (aDistance <= bDistance) {
+      return a;
+    }
+    return b;
+  }
+  let matchedViews = paneViews.filter((curr) => {
+    let currBox = curr.getBoundingClientRect();
+    switch (direction) {
+      case 'left':
+        return currBox.right <= box.left;
+      case 'right':
+        return currBox.left >= box.right;
+      case 'above':
+        return currBox.bottom <= box.top;
+      case 'below':
+        return currBox.top >= box.bottom;
+    }
+  });
+  if (matchedViews.length == 0) {
+    return null;
+  }
+  return matchedViews.reduce((prev, curr) => {
+    let prevBox = prev.getBoundingClientRect();
+    let currBox = curr.getBoundingClientRect();
+    switch (direction) {
+      case 'left':
+        if (currBox.right == prevBox.right) {
+          let result = compareVerticalDistance(currBox, prevBox);
+          return (result == currBox) ? curr : prev;
+        }
+        if (currBox.right >= prevBox.right) {
+          return curr;
+        }
+        return prev;
+      case 'right':
+        if (currBox.left == prevBox.left) {
+          let result = compareVerticalDistance(currBox, prevBox);
+          return (result == currBox) ? curr : prev;
+        }
+        if (currBox.left <= prevBox.left) {
+          return curr;
+        }
+        return prev;
+      case 'above':
+        if (currBox.bottom == prevBox.bottom) {
+          let result = compareHorizontalDistance(currBox, prevBox);
+          return (result == currBox) ? curr : prev;
+        }
+        if (currBox.bottom >= prevBox.bottom) {
+          return curr;
+        }
+        return prev;
+      case 'below':
+        if (currBox.top == prevBox.top) {
+          let result = compareHorizontalDistance(currBox, prevBox);
+          return (result == currBox) ? curr : prev;
+        }
+        if (currBox.top <= prevBox.top) {
+          return curr;
+        }
+        return prev;
+    }
+  });
+}
+atom.commands.add('atom-workspace atom-pane', 'custom:focus-pane-on-left', () => {
+  let target = neighborPaneViewInDirection('left')
+  if(!target) return;
+  target.focus();
+});
+atom.commands.add('atom-workspace atom-pane', 'custom:focus-pane-on-right', () => {
+  let target = neighborPaneViewInDirection('right')
+  if(!target) return;
+  target.focus();
+});
+atom.commands.add('atom-workspace atom-pane', 'custom:focus-pane-above', () => {
+  let target = neighborPaneViewInDirection('above')
+  if(!target) return;
+  target.focus();
+});
+atom.commands.add('atom-workspace atom-pane', 'custom:focus-pane-below', () => {
+  let target = neighborPaneViewInDirection('below')
+  if(!target) return;
+  target.focus();
+});
+
 function swapPane (direction) {
   let fromPane = atom.workspace.getActivePane();
   let fromItem = fromPane.getActiveItem();
-  document.querySelector('atom-workspace')[`focusPaneView${direction}`]();
-  let toPane = atom.workspace.getActivePane();
+  let toPaneView = neighborPaneViewInDirection(direction);
+  if (!toPaneView) return;
   
-  console.log(fromPane.id, toPane.id);
-  if (fromPane.id === toPane.id) {
-    return;
-  }
+  let toPane = toPaneView.getModel();
   let toItem = toPane.getActiveItem();
   
   if (toPane.getItems().length === 1) {
@@ -177,41 +279,38 @@ function swapPane (direction) {
   toPane.destroyInactiveItems();
   fromPane.activateItem(toItem);
   fromPane.destroyInactiveItems();
+  toPane.activate();
 }
 
 atom.commands.add('atom-workspace', 'custom:swap-pane-with-right', () => {
-  swapPane('OnRight');
+  swapPane('right');
 });
 atom.commands.add('atom-workspace', 'custom:swap-pane-with-left', () => {
-  swapPane('OnLeft')
+  swapPane('left')
 });
 atom.commands.add('atom-workspace', 'custom:swap-pane-with-up', () => {
-  swapPane('Above');
+  swapPane('above');
 });
 atom.commands.add('atom-workspace', 'custom:swap-pane-with-down', () => {
-  swapPane('Below');
+  swapPane('below');
 });
 
 function movePane (direction) {
   let fromPane = atom.workspace.getActivePane();
   let fromItem = fromPane.getActiveItem();
-  document.querySelector('atom-workspace')[`focusPaneView${direction}`]();
-  let collisionPane = atom.workspace.getActivePane();
-  
-  if (fromPane.id === collisionPane.id) {
-    return;
-  }
-  
+  let collisionPaneView = neighborPaneViewInDirection(direction);
+  if (!collisionPaneView) return;
+  let collisionPane = collisionPaneView.getModel();
   collisionPane.splitDown();
   let toPane = atom.workspace.getActivePane();
   fromPane.moveItemToPane(fromItem, toPane, 0);
 }
 
 atom.commands.add('atom-workspace', 'custom:move-pane-to-right', () => {
-  movePane('OnRight');
+  movePane('right');
 });
 atom.commands.add('atom-workspace', 'custom:move-pane-to-left', () => {
-  movePane('OnLeft');
+  movePane('left');
 });
 
 
