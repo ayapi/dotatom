ada99d08cfcea2e0c7d6318a08f2bc9bcf9d65eb
ayapi <colors.aya@gmail.com>
2016-08-26 20:55:48 +0900

    fix #22 [term] filter unnecessary env vars

--- packages-custom/ayapi-term/lib/process.js
+++ packages-custom/ayapi-term/lib/process.js
@@ -4,6 +4,18 @@ const path = require('path');
 const fs = require('fs');
 const child = require('child_process');
 
+function getFilteredEnv() {
+  let env = Object.assign({}, process.env);
+  let ignore_names = [
+    'ELECTRON_RUN_AS_NODE', 'ATOM_HOME', 'userAgent', 'taskPath'
+  ];
+  ignore_names.forEach((name) => {
+    delete env[name];
+  });
+  env.TERM = "xterm-256color";
+  return env;
+}
+
 module.exports = function(pwd, shell, args, options) {
   let callback, emitTitle, ptyProcess, title;
   options = options || {};
@@ -13,7 +25,7 @@ module.exports = function(pwd, shell, args, options) {
   }
   ptyProcess = pty.fork(shell, args, {
     cwd: pwd,
-    env: process.env,
+    env: getFilteredEnv(),
     name: 'xterm-256color',
     cols: options.cols,
     rows: options.rows
